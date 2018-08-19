aa911843f68409031f9e40f3d81a0d036430252b
Israel Parra <iparra@MacBook-Pro-de-Israel.local>
2018-08-19 11:51:06 +0200

    initial upload

--- src/store.js
+++ src/store.js
@@ -5,8 +5,16 @@ Vue.use(Vuex)
 
 import searchModule from './modules/searchModule'
 
+import VuexPersistence from 'vuex-persist'
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage,
+    key: 'buscador-vuex',
+    modules: ['searchModule']
+})
+
 export default new Vuex.Store({
   modules: {
     searchModule
-  }
+  },
+  plugins: [vuexLocal.plugin]
 })
